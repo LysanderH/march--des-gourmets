aa499f43ec6406426da8a2167b5f857189ed0db5
Hans <lysander.hans@hotmail.com>
2021-01-08 14:07:48 +0100

    📦 required whitecube/nova-page

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\NovaServiceProvider::class,
+        Whitecube\NovaPage\NovaPageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -212,6 +214,7 @@ return [
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
+        'Page' => Whitecube\NovaPage\NovaPageFacade::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
