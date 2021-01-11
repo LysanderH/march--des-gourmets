8f8e5559971c31f434c6c6a260b9f70cb5a8aa6b
Hans <lysander.hans@hotmail.com>
2021-01-11 17:00:18 +0100

    ➕ Added home page template

--- app/Nova/Templates/Home.php
+++ app/Nova/Templates/Home.php
@@ -2,12 +2,19 @@
 
 namespace App\Nova\Templates;
 
+
 use Illuminate\Http\Request;
+use Khalin\Nova\Field\Link;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Image;
+use Ebess\AdvancedNovaMediaLibrary\Fields\Images;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Panel;
 use Whitecube\NovaPage\Pages\Template;
 
 class Home extends Template
 {
-
     /**
      * Get the fields displayed by the resource.
      *
@@ -16,7 +23,14 @@ class Home extends Template
      */
     public function fields(Request $request)
     {
-        return [];
+        return [
+            Image::make('Image de fond', 'bcg_img')->hideFromIndex(),
+            new Panel('Message d’accueil', $this->welcomeMessageFields()),
+            new Panel('Premier élément disctinctif', $this->featureOneFields()),
+            new Panel('Les projets soutenus', $this->supportedProjectsFields()),
+            new Panel('Deuxième élément disctinctif', $this->featureTwoFields()),
+            new Panel('Section exposant', $this->exhibitorCTAFields()),
+        ];
     }
 
     /**
@@ -28,5 +42,77 @@ class Home extends Template
     public function cards(Request $request)
     {
         return [];
+    }
+
+    /**
+     * Groups the fields for the welcome message
+     * on homepage
+     *
+     */
+    public function welcomeMessageFields()
+    {
+        return [
+            Text::make('Titre du message d’accueil', 'welcome_title')->hideFromIndex(),
+            Textarea::make('Texte du message d’accueil', 'welcome_text')->hideFromIndex()
+        ];
+    }
+
+    /**
+     * Groups the fields for the first feature
+     * on homepage
+     *
+     */
+    public function featureOneFields()
+    {
+        return [
+            Text::make('Titre', 'feature_title'),
+            Textarea::make('Contenu', 'feature_content'),
+            Image::make('Ajouter une image', 'feature_img')
+                ->help('Veuillez choisir une image qui fait moins de 1200x1200 et moins de 2 Mo.'),
+            Text::make('Label du lien', 'feature_link_label'),
+            Link::make('Url', 'feature_link'),
+            Boolean::make('Ouvrir le lien dans un nouvel onglet', 'feature_blank')
+        ];
+    }
+
+    public function supportedProjectsFields()
+    {
+        return [
+            Text::make('1. Titre', 'supported_title_one'),
+            Textarea::make('1. Contenu', 'supported_content_one'),
+            Image::make('1. Ajouter une image', 'supported_img_one'),
+            Text::make('2. Titre', 'supported_title_two'),
+            Textarea::make('2. Contenu', 'supported_content_two'),
+            Image::make('2. Ajouter une image', 'supported_img_two'),
+            Text::make('3. Titre', 'supported_title_three'),
+            Textarea::make('3. Contenu', 'supported_content_three'),
+            Image::make('3. Ajouter une image', 'supported_img_three'),
+            Text::make('Label du lien', 'supported_link_label'),
+            Link::make('Url', 'supported_link')->placeholder('https://www.exemple.com'),
+            Boolean::make('Ouvrir le lien dans un nouvel onglet', 'supported_blank')
+        ];
+    }
+
+    public function featureTwoFields()
+    {
+        return [
+            Text::make('Titre', 'feature_two_title'),
+            Textarea::make('Contenu', 'feature_two_content'),
+            Image::make('Ajouter une image', 'feature_two_img'),
+            Text::make('Label du lien', 'feature_two_link_label'),
+            Link::make('Url', 'feature_two_link'),
+            Boolean::make('Ouvrir le lien dans un nouvel onglet', 'feature_two_blank')
+        ];
+    }
+
+    public function  exhibitorCTAFields()
+    {
+        return [
+            Text::make('Titre', 'exhibitor_cta_title'),
+            Image::make('Ajouter une image', 'exhibitor_cta_img'),
+            Text::make('Label du lien', 'exhibitor_cta_link_label'),
+            Link::make('Url', 'exhibitor_cta_link'),
+            Boolean::make('Ouvrir le lien dans un nouvel onglet', 'exhibitor_cta_blank')
+        ];
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 
-@section('main-heading', 'Accueil')
+@section('main-heading', @pageTitle)
 
 @section('content')
     <div class="landing-zone">
@@ -9,15 +9,12 @@
                 <h2 role="heading" aria-level="2" class="subtitle hello__heading" id="hello-heading">Bienvenue à la
                     20<sup>ème</sup>
                     édition</h2>
-                <p class="hello__text">Le Marché des Gourmets est un événement annuel organisé par le Rotary Club de
-                    Flémalle
-                    sur
-                    le prestigieux site de l'abbaye de la Paix-Dieu afin d'obtenir des fonds pour aider les plus démunis.
-                    Nous
-                    rassemblons pour vous des producteurs de qualité provenant de la région.</p>
+                <p class="hello__text">{{ Page::get('welcome_text') }}</p>
                 <a href="{{ route('gallery') }}" class="hello__link cta btn">Voir la galerie</a>
             </section>
-            <div class="landing-zone__img"></div>
+            <div class="landing-zone__img" style="background-image: url({{ asset('storage/' . Page::get('bcg_img')) }})"
+                 aria-hidden="true">
+            </div>
             <div class="hide_1" aria-hidden="true"></div>
             <div class="hide_2" aria-hidden="true"></div>
             <div class="hide_3" aria-hidden="true"></div>
@@ -41,49 +38,45 @@
     </section>
 
     <section class="feature" aria-label="La garderie">
-        <h2 class="feature__heading" role="heading" aria-level="2">La garderie</h2>
-        <img src="{{ asset('../storage/assets/img/pexels-mentatdgt-1049622.jpg') }}" alt="Image de la garderie"
+        <h2 class="feature__heading" role="heading" aria-level="2">{{ Page::get('feature_title') }}</h2>
+        <img src="{{ asset('/storage/' . Page::get('feature_img')) }}" alt="Image de la garderie"
              class="feature__img" width="536" height="458">
-        <p class="feature__description">Pour les enfants de _ à _ ans, les membre du _________ organisent une garderie
-            avec des animations et des jeux. Ainsi, il vous est possible de passer un beau moment tout en sachant que vos
-            enfants s'amusent.</p>
-        <a href="/plan-d-espace" class="feature__link cta btn">Voir le plan d’espace</a>
+        <p class="feature__description">{{ Page::get('feature_content') }}</p>
+        <a href="{{ Page::get('feature_link') }}" class="feature__link cta btn"
+           @if (Page::get('feature_blank')) rel="noopener noreferrer" target="_blank"
+            @endif>{{ Page::get('feature_link_label') }}</a>
     </section>
 
     <section class="supported" aria-label="Porjets soutenus">
         <h2 class="supported__heading" role="heading" aria-level="2">Porjets soutenus</h2>
         <article class="supported__item">
-            <h3 class="supported__sect-heading">La construction de la paix</h3>
-            <img src="{{ asset('/storage/assets/img/rc-desease.jpg') }}" alt="" class="supported__img">
-            <p class="supported__text">Le Rotary lance la conversation pour favoriser la compréhension mutuelle malgré les
-                différences culturelles. Nous formons des adultes et des jeunes à la prévention et à la médiation des
-                conflits, et nous venons en aide aux réfugiés.</p>
+            <h3 class="supported__sect-heading">{{ Page::get('supported_title_one') }}</h3>
+            <img src="{{ asset('/storage/' . Page::get('supported_img_one')) }}" alt="" class="supported__img">
+            <p class="supported__text">{{ Page::get('supported_content_one') }}</p>
         </article>
         <article class="supported__item">
-            <h3 class="supported__sect-heading">Approvisionnement en eau potable, assainissement et hygiène </h3>
-            <img src="{{ asset('/storage/assets/img/rc-water.jpg') }}" alt="" class="supported__img">
-            <p class="supported__text">Nous soutenons les solutions locales pour approvisionner en eau potable et donner
-                accès à l’assainissement et à l’hygiène au plus grand nombre. Nous partageons nos expertises avec les
-                décideurs et les éducateurs locaux pour nous assurer de la pérennité de nos actions.</p>
+            <h3 class="supported__sect-heading">{{ Page::get('supported_title_two') }}</h3>
+            <img src="{{ asset('/storage/' . Page::get('supported_img_two')) }}" alt="" class="supported__img">
+            <p class="supported__text">{{ Page::get('supported_content_two') }}</p>
         </article>
         <article class="supported__item">
-            <h3 class="supported__sect-heading">Lutte contre les maladies</h3>
-            <img src="{{ asset('/storage/assets/img/peace.jpg') }}" alt="" class="supported__img">
-            <p class="supported__text">Nous éduquons et équipons les collectivités afin de stopper la propagation de
-                maladies mortelles telles que la polio, le sida ou le paludisme. Nous améliorons l’accès aux soins dans les
-                pays en développement.</p>
+            <h3 class="supported__sect-heading">{{ Page::get('supported_title_three') }}</h3>
+            <img src="{{ asset('/storage/' . Page::get('supported_img_three')) }}" alt="" class="supported__img">
+            <p class="supported__text">{{ Page::get('supported_content_three') }}</p>
         </article>
-        <a href="" class="supported__btn cta btn" rel="noopener noreferrer" target="_blank">Savoir plus sur nos projets</a>
+        <a href="{{ Page::get('exhibitor_cta_link') }}" class="supported__btn cta btn"
+           @if (Page::get('exhibitor_cta_blank')) rel="noopener noreferrer" target="_blank"
+            @endif>{{ Page::get('exhibitor_cta_link_label') }}</a>
     </section>
 
     <section class="feature" aria-label="Le restaurant">
-        <h2 class="feature__heading" role="heading" aria-level="2">Le restaurant</h2>
-        <img src="{{ asset('../storage/assets/img/pexels-elevate-1267320.jpg') }}" alt="Image de la garderie"
+        <h2 class="feature__heading" role="heading" aria-level="2">{{ Page::get('feature_two_title') }}</h2>
+        <img src="{{ asset('/storage/' . Page::get('feature_two_img')) }}" alt="Image de la garderie"
              class="feature__img" width="536" height="458">
-        <p class="feature__description">Les membres du rotari club flémalle organisent un restaurant avec des plats froids,
-            dont les ingrédients proviennent des producteurs du marché. Ainsi, vous pouvez gouter l'excellence des produits
-            offerts à nos stands.</p>
-        <a href="{{ route('exhibitors') }}" class="feature__link cta btn">Voir les exposants</a>
+        <p class="feature__description">{{ Page::get('feature_two_content') }}</p>
+        <a href="{{ Page::get('feature_two_link') }}" class="feature__link cta btn"
+           @if (Page::get('feature_two_blank')) rel="noopener noreferrer" target="_blank"
+            @endif>{{ Page::get('feature_two_link_label') }}</a>
     </section>
 
     <section class="pricing" aria-label="Les prix">
@@ -107,8 +100,10 @@
     </section>
 
     <section class="producer" aria-label="Voulez-vous vendre vos produits?">
-        <h2 class="producer__heading" role="heading" aria-level="2">Voulez-vous vendre vos produits?</h2>
+        <h2 class="producer__heading" role="heading" aria-level="2">{{ Page::get('exhibitor_cta_title') }}</h2>
         <img src="{{ asset('/storage/assets/img/hello.jpeg') }}" alt="Image représentant la salle d'exposition">
-        <a href="" class="btn cta producer__btn">Inscrivez-vous ici</a>
+        <a href="{{ Page::get('exhibitor_cta_link') }}" class="btn cta producer__btn"
+           @if (Page::get('exhibitor_cta_blank')) rel="noopener noreferrer" target="_blank"
+            @endif>{{ Page::get('exhibitor_cta_link_label') }}</a>
     </section>
 @endsection
